8a194c6ce4737c916890ad5ddc9321b17808c6a0
RaeYang <s10243305@connect.np.edu.sg>
2023-06-30 16:16:02 +0800

    ver2.0

--- app/src/main/java/com/example/myapplication/User.java
+++ app/src/main/java/com/example/myapplication/User.java
@@ -1,2 +1,15 @@
-package com.example.myapplication;public class User {
+package com.example.myapplication;
+
+public class User {
+    String name;
+    String description;
+    int id;
+    boolean followed;
+
+    public User(String Name, String Description, int Id, boolean Followed){
+        Name = name;
+        Description = description;
+        Id = id;
+        Followed = followed;
+    }
 }
